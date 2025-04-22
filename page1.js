396ac3691204c73a5f7335a1792b1d1c1b8d7e97
Nil0576 <118661358+Nil0576@users.noreply.github.com>
2025-04-22 10:55:43 +0530

    Update page1.js

--- page1.js
+++ page1.js
@@ -18,12 +18,13 @@ import {
 
 // Firebase App Config (replace with your actual config)
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDW0kotXcUTyWf-XpvG4wDeEX-8euvCY3M",
+  authDomain: "to-do-e4fc2.firebaseapp.com",
+  projectId: "to-do-e4fc2",
+  storageBucket: "to-do-e4fc2.firebasestorage.app",
+  messagingSenderId: "613208522123",
+  appId: "1:613208522123:web:f6dd4b0b259cdb038b438e",
+  measurementId: "G-V6KP95H4PL"
 };
 
 const app = initializeApp(firebaseConfig);
